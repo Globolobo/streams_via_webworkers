09ca3a20d1837303f7ec1ebe16c8daaf67b9583a
globolobo <sean.pichardo@gmail.com>
2018-06-20 02:15:04 -0700

    finally... finished pagination

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import requestObj from './request/request';
-import pagination from './streams/pagination/index';
+import builtStreamObjHtml from './streams/streamContainer/index';
 import store from './store';
+import paginationFn from './streams/pagination/pagination';
 
 document.getElementById('searchForm').addEventListener('submit', async function(e){
     e.preventDefault();
@@ -8,18 +9,20 @@ document.getElementById('searchForm').addEventListener('submit', async function(
     
     store.set('currQuery', query);
     
-    if(!store.has(query)){
-      const storeBool = await requestObj(query);
-      if(!storeBool){
+      const res = store.has(query) ? true : requestObj(query);
+      if(!res){
         return alert('Issue with Store');
       }
-    }
-    pagination();
+      res.then(()=>{
+      paginationFn();    
+      builtStreamObjHtml();
+      const currStreamKey = store.get('currQuery');
+      const currQueryMap = store.get(currStreamKey);
+      const currQueryTotal = currQueryMap.get('total')
+      document.getElementById('totalResults').innerHTML = currQueryTotal; 
+    })
 
-    const currStreamKey = store.get('currQuery');
-    const currQueryMap = store.get(currStreamKey);
-    const currQueryTotal = currQueryMap.get('total')
-    document.getElementById('totalResults').innerHTML = currQueryTotal;
-
+    
+    
   });
 
